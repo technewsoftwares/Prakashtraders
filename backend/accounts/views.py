4a53258641feab26b15ea8d881361911c6df02f6
Tech New Softwares <technewsoftwares@gmail.com>
2026-07-28 22:41:34 +0530

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -135,41 +135,41 @@ class SendOTP(APIView):
         OTP.objects.create(email=email, otp=otp)
 
 
-try:
-    print("STEP 1")
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=10)
-
-    print("STEP 2")
-
-    server.login(
-        settings.EMAIL_HOST_USER,
-        settings.EMAIL_HOST_PASSWORD
-    )
-
-    print("STEP 3")
-
-    server.sendmail(
-        settings.EMAIL_HOST_USER,
-        email,
-        f"Subject: OTP\n\nYour OTP is {otp}"
-    )
-
-    print("STEP 4")
-
-    server.quit()
-
-except Exception as e:
-    import traceback
-
-    traceback.print_exc()
-
-    return Response(
-        {
-            "error": str(e)
-        },
-        status=500
-    )
+        try:
+            print("STEP 1")
+        
+            server = smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=10)
+        
+            print("STEP 2")
+        
+            server.login(
+                settings.EMAIL_HOST_USER,
+                settings.EMAIL_HOST_PASSWORD
+            )
+        
+            print("STEP 3")
+        
+            server.sendmail(
+                settings.EMAIL_HOST_USER,
+                email,
+                f"Subject: OTP\n\nYour OTP is {otp}"
+            )
+        
+            print("STEP 4")
+        
+            server.quit()
+        
+        except Exception as e:
+            import traceback
+        
+            traceback.print_exc()
+        
+            return Response(
+                {
+                    "error": str(e)
+                },
+                status=500
+            )
         return Response({
             "success": True,
             "message": "OTP sent successfully"
