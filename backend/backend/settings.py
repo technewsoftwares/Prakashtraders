1776906bc9d196af0f66a84008c0f361c26c986a
Tech New Softwares <technewsoftwares@gmail.com>
2026-07-28 22:12:19 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,7 +125,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
