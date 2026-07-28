9024573fab0c6df60ef55ecb4e21f9b8fa694395
nssathish37 <technewsoftwares@gmail.com>
2026-07-28 10:36:22 +0530

    Configure settings for Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'backend_db'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Prakashtraders@1984'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
