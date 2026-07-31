df5c1aa8208ef18b628de143541bf517433a4ba1
nssathish37 <technewsoftwares@gmail.com>
2026-07-31 14:26:40 +0530

    Register Product model in Django admin

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "price",
+        "stock",
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+        "category",
+        "brand",
+    )
+
+    list_filter = (
+        "category",
+        "is_active",
+        "is_best_product",
+    )
