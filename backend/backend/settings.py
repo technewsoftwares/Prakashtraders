ef02ab480805b6f5e120288efa4e2ec732b41411
Tech New Softwares <technewsoftwares@gmail.com>
2026-07-31 21:17:28 +0530

    Update settings.py by kabi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,8 +136,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
 
 EMAIL_TIMEOUT = 10
 
