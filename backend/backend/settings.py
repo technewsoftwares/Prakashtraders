8e0cd2c90b81ad872d6d44d5d4a22f990efa38e2
Tech New Softwares <technewsoftwares@gmail.com>
2026-07-31 20:56:51 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,13 +130,15 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
 EMAIL_TIMEOUT = 10
 
 
