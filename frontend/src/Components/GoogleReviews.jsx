63a40d8b6b7ac993012c73514450f90fa679c1c3
nssathish37 <technewsoftwares@gmail.com>
2026-08-02 08:45:50 +0530

    Update Google Reviews Page

--- frontend/src/Components/GoogleReviews.jsx
+++ frontend/src/Components/GoogleReviews.jsx
@@ -1,7 +1,7 @@
 const GoogleReviews = () => {
   return (
     <section className="bg-black py-12">
-      <div className="w-full px-2">
+      <div className="max-w-[94%] mx-auto">
 
         <h2 className="text-white text-4xl font-bold text-center uppercase mb-10">
           ⭐ What Our Customers Say
