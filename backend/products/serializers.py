b5150188d3362b8856f692f11019577466283e09
Tech New Softwares <technewsoftwares@gmail.com>
2026-09-05 17:49:53 +0530

    Update serializers.py by kabilan

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
 
 
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "category",
+            "brand",
+            "original_price",
+            "price",
+            "discount_price",
+            "stock",
+            "rating",
+            "reviews_count",
+            "image_1",
+            "image_2",
+            "image_3",
+        ]
