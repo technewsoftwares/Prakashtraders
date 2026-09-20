b1cae83df5e7cc51abe2f18f45801543a2cfc746
Tech New Softwares <technewsoftwares@gmail.com>
2026-09-20 13:28:26 +0530

    Update urls.py

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_order, verify_payment, payment_webhook, admin_orders, delete_order, UserOrdersView, track_order
+from .views import create_order, verify_payment, payment_webhook, admin_orders, delete_order, UserOrdersView, track_order, update_order_tracking
 
 urlpatterns = [
     path("create-order/", create_order),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("admin-orders/delete/<str:order_id>/", delete_order),
     path("my-orders/", UserOrdersView.as_view()),
     path("track/<str:order_id>/", track_order, name="track-order"),
+    path("admin/update-tracking/<str:order_id>/", update_order_tracking, name="admin-update-order-tracking"),
 ]
